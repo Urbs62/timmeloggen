a64e55e876368dbf2cb133a3a63e814912571a27
Urbs62 <urban@sandlund.org>
2026-02-04 19:31:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* TimeLedger PWA Service Worker (cache-first för app-skalet) */
-const CACHE_NAME = "timmeloggen-v31";
+const CACHE_NAME = "timmeloggen-v40";
 const ASSETS = [
   "./",
   "./index.html",
