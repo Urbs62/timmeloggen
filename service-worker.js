7f918681914d97c5833ed18d270ca4106710d5db
Urbs62 <urban@sandlund.org>
2026-02-21 13:57:47 +0100

    Bumpade

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* TimeLedger PWA Service Worker (cache-first för app-skalet) */
-const CACHE_NAME = "timmeloggen-v3.1";
+const CACHE_NAME = "timmeloggen-v3.2";
 const ASSETS = [
   "./",
   "./index.html",
