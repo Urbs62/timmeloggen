4ed1951b5b2db27b9607805b001f887d36b63fae
Urbs62 <urban@sandlund.org>
2026-02-23 17:26:02 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+const SETTINGS_KEY = "tl_settings_v1";
+
+function loadSettings(){
+  try{
+    const raw = localStorage.getItem(SETTINGS_KEY);
+    return raw ? JSON.parse(raw) : {};
+  }catch{
+    return {};
+  }
+}
+
+function saveSettings(s){
+  localStorage.setItem(SETTINGS_KEY, JSON.stringify(s));
+}
